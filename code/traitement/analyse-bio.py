29cd61c559de6c6c0c713c8a5a7bf75d85a9bc30
faureleon <leon.faure@cri-paris.org>
2017-01-23 12:57:48 +0100

    finalV

--- code/traitement/analyse-bio.py
+++ code/traitement/analyse-bio.py
@@ -6,6 +6,8 @@ time501, velocity501 = np.genfromtxt("data-50%-1.csv", unpack=True, delimiter=",
 
 time502, velocity502 = np.genfromtxt("data-50%-2.csv", unpack=True, delimiter=",", skip_header=2)
 
+time503, velocity503 = np.genfromtxt("50%-3.csv", unpack=True, delimiter=",", skip_header=2)
+
 time751, velocity751 = np.genfromtxt("data75%-1.csv", unpack=True, delimiter=",", skip_header=2)
 
 time752, velocity752 = np.genfromtxt("data75%-2.csv", unpack=True, delimiter=",", skip_header=2)
@@ -22,6 +24,7 @@ timeNoir, velocityNoir = np.genfromtxt("Noir.csv", unpack=True, delimiter=",", s
 
 Velocity501 = velocity501 / 1.8
 Velocity502 = velocity502 / 1.8
+Velocity503 = velocity503 / 1.8
 Velocity751 = velocity751 / 1.8
 Velocity752 = velocity752 / 1.8
 Velocity251 = velocity251 / 1.8
@@ -38,6 +41,7 @@ plt.ylabel("Velocity of the gas release in cm.min⁻¹")
 plt.title("Velocity of the gas release according to time - 50% LED intensity")
 plt.plot(time501, Velocity501, "b", label="Replicate 1")
 plt.plot(time502, Velocity502, "c", label="Replicate 2")
+plt.plot(time502, Velocity503, "m", label="Replicate 3")
 plt.ylim([0,0.8])
 plt.legend()
 
